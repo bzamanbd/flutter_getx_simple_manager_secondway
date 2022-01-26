f744cf60d3dcb5b625388f7ba873ef391b6a989c
bzamanbd <riponzamanbd@gmail.com>
2022-01-27 01:22:02 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_getx_simple_manager_secondway/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_getx_simple_manager_secondway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../getx_controllers/number_controller.dart';
+import 'package:get/get.dart';
+
+// ignore: must_be_immutable
+class HomeScreen extends StatelessWidget {
+  final String title;
+  HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  NumberController numberController = NumberController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GetBuilder<NumberController>(
+              init: numberController,
+              builder: (controller)=>Text(
+              'The Number is : ${controller.count}',
+              textScaleFactor: 1.3,
+            ),
+            ),
+            const SizedBox(height: 26),
+            ElevatedButton(
+                onPressed: ()=> numberController.increment(),
+                child: const Text(
+                  'Next',
+                  textScaleFactor: 1.4,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
